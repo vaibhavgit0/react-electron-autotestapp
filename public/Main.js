bab90364e34cf21e03aaa3c37b9c69c264035af2
vaibhavgit0 <vaibhav@prosperasoft.com>
2020-05-06 17:39:02 +0530

    electron app file updated

--- public/Main.js
+++ public/Main.js
@@ -1,11 +1,46 @@
 const {app, BrowserWindow} = require('electron')
+const express = require('express');
+const exp = express();
+const PORT = 4000;
+const bodyParser = require('body-parser');
+//const nrc = require('node-run-cmd');
+var child = require('child_process').exec;
+
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({width: 800, height: 600})
 
-        // and load the index.html of the app.
-        //win.loadFile('index.html')
-        win.loadURL('http://localhost:3000/')
+    // and load the index.html of the app.
+    //win.loadFile('index.html')
+    win.loadURL('http://localhost:3000/')
 
 }
+
+exp.use(bodyParser.json());
+exp.use(bodyParser.urlencoded({
+    extended: true
+}));
+exp.post('/execCommand', function(req, res) {
+    var name = req.body.suites;
+    var executablePath = "java -jar testcase/TMSTestNG-0.0.1-SNAPSHOT.jar "+name;
+    child(executablePath, [], function(err, data) {
+        console.log(err)
+        console.log(data.toString());
+
+    });
+    res.send({"body": "success"});
+
+    /*console.log(req.body);
+    //res.send(req.body);
+    name = req.body.suites;
+
+    nrc.run(['java -jar testcase/TMSTestNG-0.0.1-SNAPSHOT.jar '+name, 'rm -rf public/test-output', 'mv test-output public/']).then(function(exitCodes) {
+         res.send({"body": "success"});
+    }, function(err) {
+          console.log('Command failed to run with error: ', err);
+
+    });*/
+});
+
+const server = exp.listen(4000);
 app.on('ready', createWindow)
--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import GlobalSearchComponent from "./GlobalSearchComponent";
 import logo from './gear.png';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
+const axios = require('axios');
 
 class App extends Component{
     componentDidMount() {
@@ -25,7 +28,9 @@ class App extends Component{
             flag: false,
             toggle:false,
             data: [],
-            filteredData: []
+            filteredData: [],
+            load:false,
+            output:false
         }
         this.fetchFile = this.fetchFile.bind(this);
     }
@@ -50,15 +55,91 @@ class App extends Component{
         this.setState({
             toggle : true,
             name : value[0],
-            desc : value[1]
+            desc : value[1],
+            suite : value[2],
+            output:false,
+            load:true
         })
+        setTimeout(() => {
+            this.setState({
+                load:false
+            })
+        }, 1000);
     }
-    executeCommand(e, value) {
+    executeCommand(e, value, suites) {
+        var state = this;
         e.preventDefault();
-        alert("Executing test Case :"+ value);
+        this.setState({
+            load:true
+        })
+
+        alert("Executing test case :"+ value);
+
+        axios
+        .post("/execCommand", {suites})
+        .then(response => {
+            console.log(response.data);
+            if(response.data.body == "success") {
+                alert("Testcase Executed Succesfully !!")
+                state.setState({
+                    load:false,
+                    output:true
+                })
+            }
+        }).
+        catch(err => {
+            console.log(err.code);
+            console.log(err.message);
+            alert("something failed !!")
+            state.setState({
+                load:false,
+                //output:true
+            })
+        })
+
+      /*fetch("/execCommand", {
+          method: 'POST',
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+            suites
+          })
+      }).then(function(response) {
+          if (response.status >= 400) {
+            throw new Error("Bad response from server");
+          }
+          return response.json();
+      }).then(function(data) {
+          if(data.body == "success") {
+              alert("Testcase Executed Succesfully !!")
+              state.setState({
+                  load:false,
+                  output:true
+              })
+          }
+          else {
+              alert("Something Failed !!")
+              state.setState({
+                  load:false,
+                  output:true
+              })
+          }
+      }).catch(function(err) {
+          console.log(err)
+          alert("Please wait..")
+          setTimeout(() =>  {
+              state.setState({
+                  load:false,
+                  output:true
+              })
+              alert("Testcase Executed!!")
+
+          }, 60000)
+      });*/
+
     }
+
     renderDetails() {
-        const { toggle, name, desc } = this.state;
+        const { toggle, name, desc, suite } = this.state;
         if(!toggle) {
             return (
                     <div className="FormCenter">
@@ -77,7 +158,7 @@ class App extends Component{
                                         <p><strong>Description:</strong> {desc}</p>
                                     </div>
                                     <div>
-                                        <button className="FormField__Button mr-20" onClick={(e) => this.executeCommand(e, name)}>Run</button>
+                                        <button className="FormField__Button mr-20" onClick={(e) => this.executeCommand(e, name, suite)}>Run</button>
                                     </div>
                                 </div>
                             </div>
@@ -103,8 +184,16 @@ class App extends Component{
                         <h1 className="App-title">AutoTest</h1>
                         </div>
                     </header>
-
+                    <div id={this.state.load ? 'overlay' : ''}></div>
                     <div className="App">
+                    <div className="App_Align">
+                        {this.state.load && <Loader
+                            type="Oval"
+                            color="#52C4B9"
+                            height={100}
+                            width={100}
+                        />}
+                    </div>
                         <div className="App__Aside">
                             <GlobalSearchComponent
                                 data={this.state.data}
@@ -116,7 +205,7 @@ class App extends Component{
                                 this.state.filteredData.map((numList,i, val) =>(
                                     val = numList.split(","),
                                     <tr id={i} onClick={(e) => this.getDetails(e, val)}>
-                                    <td>{val[0]}</td></tr>
+                                    <td disabled={this.state.load}>{val[0]}</td></tr>
 
                                 ))
                             }
@@ -128,12 +217,20 @@ class App extends Component{
                                 <h1>Details</h1>
                                 {this.renderDetails()}
                             </div>
-                            <div className="App__Data">
+                            { this.state.output && <div className="App__Data">
+                                <h1>Output</h1>
+                                <div className="FormCenter">
+                                    <iframe src="./test-output/index.html" width="800" height="400"></iframe>
+                                </div>
+                            </div>}
+                            {/*<div className="App__Data">
                                 <h1>Previous Runs</h1>
                                 <div className="FormCenter">
                                     Previous runs of testcases will be displayed here.
                                 </div>
-                            </div>
+                            </div>*/}
+                            <br/>
+                            <button className="FormField__Button mr-20" onClick={(e) => this.executeCommand(e, " All", "All.xml")}>Run All</button>
                         </div>
                     </div>
                 </div>
